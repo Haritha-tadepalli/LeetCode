9c6592306156c9f1f637d62e4720089f2c02eda0
Haritha-tadepalli <t.haritha032@gmail.com>
2023-01-26 09:37:33 +0530

    Added square root of x

---
+++ Easy/69.Sqrt-x.cpp
@@ -0,0 +1,28 @@
+/*
+Question: https://leetcode.com/problems/sqrtx/
+Solution:
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	int mySqrt(int x) {
+		int i = 0, j = x, ans;
+		while(i <= j){
+			long long int mid=i + (j - i) / 2;
+			if(mid * mid == x){
+                ans = mid; 
+                break;
+            }
+			else if(mid * mid < x){
+                i = mid + 1;
+            }
+			else{
+                j = mid - 1;
+            }
+		}
+		return ans;
+	}
+};
