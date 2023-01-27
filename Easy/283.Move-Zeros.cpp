10c2f49e9ce6a1b53a1beba8cd9447247712162d
Haritha-tadepalli <t.haritha032@gmail.com>
2023-01-27 13:36:33 +0530

    Added Move Zeros

---
+++ Easy/283.Move-Zeros.cpp
@@ -0,0 +1,15 @@
+/*
+Question: https://leetcode.com/problems/move-zeroes/
+Solution: https://leetcode.com/problems/move-zeroes/submissions/875210318/
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+            if (nums[cur] != 0) {
+                swap(nums[lastNonZeroFoundAt++], nums[cur]);
+            }
+        }   
+    }
+};
