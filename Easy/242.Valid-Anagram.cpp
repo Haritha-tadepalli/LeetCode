3e0e8184d93064cf958512967959540044a2eac9
Haritha-tadepalli <t.haritha032@gmail.com>
2023-01-27 13:30:11 +0530

    Added Valid Anagram

---
+++ Easy/242.Valid-Anagram.cpp
@@ -0,0 +1,15 @@
+/*
+Question: https://leetcode.com/problems/valid-anagram/
+Solution: https://leetcode.com/problems/valid-anagram/submissions/875256769/
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
