8cf1fd7abbcf70674d00211e029d72fff4841eed
Haritha-tadepalli <t.haritha032@gmail.com>
2023-01-31 17:38:37 +0530

    Added Add two numbers

---
+++ Easy/2235.Add-Two-numbers.cpp
@@ -0,0 +1,11 @@
+/*
+Question: https://leetcode.com/problems/add-two-integers/
+Solution: https://leetcode.com/problems/add-two-integers/submissions/888706781/
+*/
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
