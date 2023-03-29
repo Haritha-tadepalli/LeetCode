6ad8a739024d3dcc35851e8c909f15928c83251f
Haritha-tadepalli <t.haritha032@gmail.com>
2023-03-29 18:18:06 +0530

    Added rotate image

---
+++ Medium/48.rotate-image.cpp
@@ -0,0 +1,21 @@
+/*
+Question: https://leetcode.com/problems/rotate-image/
+Solution: https://leetcode.com/problems/rotate-image/submissions/924210551/
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < n; i ++){
+            for(int j = 0; j < n / 2; j++)
+                swap(matrix[i][j], matrix[i][n - j - 1]);
+        }
+        return;
+    }
+};
